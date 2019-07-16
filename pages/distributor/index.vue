51a70abd2afc5f30ea988ab21a5b68d70347624c
Budi Purnomo <budipurnomo@Budis-MacBook-Pro.local>
2019-07-16 10:29:23 +0700

    distributor page

--- pages/distributor/index.vue
+++ pages/distributor/index.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
-    <div class="container">
-      <div class="form">
-        Welcome to Skytech
+    <componentLogo class="mt-4" />
 
-        <a a href="https://docs.google.com/forms/d/e/1FAIpQLSdck1kf4u96XEtiptimtQnnUOOw27QMHfcnRPNr9MGWUlGW8Q/viewform?usp=sf_link" class="btn btn-primary">
-          Lengkapi Data
-          <i class="fa fa-angle-right" />
+    <form @submit.prevent="login()">
+      <h4 class="mb-3 text-center">
+        Selamat datang di Skytech.id
+      </h4>
+
+      <div class="form-group py-4 text-center">
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdck1kf4u96XEtiptimtQnnUOOw27QMHfcnRPNr9MGWUlGW8Q/viewform?usp=sf_link" class="btn btn-block btn-primary" >
+          Bergabung Sekarang <i class="fa fa-angle-right" />
         </a>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
-export default {
+import componentLogo from './fragments/logo'
 
+export default {
+  layout: 'blankCentered',
+  components: {
+    componentLogo
+  }
 }
 </script>
 
