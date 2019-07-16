3b862ad4685c08a2dc600280ae851f5472d9f263
Budi Purnomo <budi-purnomo@outlook.com>
2019-07-16 11:32:39 +0700

    facelift

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,19 @@
       <li><span></span></li>
       <li><span></span></li>
       <li><span></span></li>
-      <li><span></span></li>
-      <li><span></span></li>
     </ul>
+    <div class="row pt-3 w-100 ml-0 align-item-center bg-primary footer-title" style="position:absolute; bottom:0px;">
+      <div class="col-6">
+        <h6 class="text-white mt-2">
+          SKYTECH.ID
+        </h6>
+      </div>
+      <div class="col-6 text-right">
+        <p class="text-white small">
+          Lenmarc Mall Surabaya<br>Lt. UG no. 33-35
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
