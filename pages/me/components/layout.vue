c2e75b7fa01416dd0d0c940b2a6138cb01c53237
Budi Purnomo <budi-purnomo@outlook.com>
2019-09-10 16:34:31 +0700

    update menu profile - responsive

--- pages/me/components/layout.vue
+++ pages/me/components/layout.vue
@@ -7,7 +7,7 @@
             <li :class="activeIndex === 'saya' ? 'active' : ''" @click="$router.push({ path: '/me' })">
               <i class="fa fa-user fa-lg" />
               Saya
-              <small v-show="! $store.getters['modules/uac/isProfileDataCompleted']">
+              <small v-show="!$store.getters['modules/uac/isProfileDataCompleted']">
                 <i class="fa fa-asterisk pull-right text-danger" style="padding-top: .3rem;" />
               </small>
             </li>
@@ -33,8 +33,20 @@
           </ul>
         </div>
       </div>
-      <div class="d-block d-sm-none pb-3 col-12">
-        mobile only
+      <div class="d-block d-sm-none pb-3 col-12 pb-4">
+        <div class="card hover p-3" @click="openMenu()">
+          <div class="row">
+            <div class="col-10">
+              <p class="mb-0 text-capitalize">
+                <span class="fa fa-bars" />
+                &nbsp; {{ activeIndex.replace('_', ' ') }}
+              </p>
+            </div>
+            <div class="col-2 text-right">
+              <i v-show="has_invoice || !$store.getters['modules/uac/isProfileDataCompleted']" class="fa fa-asterisk pull-right text-danger" style="padding-top: .3rem;" />
+            </div>
+          </div>
+        </div>
       </div>
       <div class="pr-4 d-none d-sm-block" />
       <div class="col">
@@ -70,6 +82,56 @@
             <a href="javascript:void(0);" class="btn btn-block btn-outline-primary" @click="$bvModal.hide('bv-modal-logout')">
               Batal
             </a>
+          </div>
+        </div>
+      </div>
+    </b-modal>
+    <b-modal
+      id="bv-modal-menu"
+      ref="modal-menu"
+      centered
+      hide-footer
+      no-close-on-esc
+      no-close-on-backdrop
+    >
+      <div class="container-fluid mb-4">
+        <div class="row">
+          <div class="col-12 pb-3 text-center">
+            <h4>Menu</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="card px-2" style="cursor:pointer;">
+              <ul class="profile-menu-large">
+                <li :class="activeIndex === 'saya' ? 'active' : ''" @click="$router.push({ path: '/me' })">
+                  <i class="fa fa-user fa-lg" />
+                  Saya
+                  <small v-show="!$store.getters['modules/uac/isProfileDataCompleted']">
+                    <i class="fa fa-asterisk pull-right text-danger" style="padding-top: .3rem;" />
+                  </small>
+                </li>
+                <li :class="activeIndex === 'wishlist' ? 'active' : ''" @click="$router.push({ path: '/me/wishlist' })">
+                  <i class="fa fa-heart-o fa-lg" />
+                  Wishlist
+                </li>
+                <li :class="activeIndex === 'tagihan_pembelian' ? 'active' : ''" @click="$router.push({ path: '/me/invoices' })">
+                  <i class="fa fa-file-text-o fa-lg" />
+                  Tagihan Pembelian
+                  <small>
+                    <i v-show="has_invoice" class="fa fa-asterisk pull-right text-danger" style="padding-top: .3rem;" />
+                  </small>
+                </li>
+                <li :class="activeIndex === 'riwayat_pembelian' ? 'active' : ''" @click="$router.push({ path: '/me/transactions' })">
+                  <i class="fa fa-history fa-lg" />
+                  Riwayat Pembelian
+                </li>
+                <li @click="confirmLogout()">
+                  <i class="fa fa-circle-o-notch fa-lg" />
+                  Logout
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -114,6 +176,9 @@ export default {
   methods: {
     confirmLogout() {
       this.$refs['modal-logout-confirm'].show()
+    },
+    openMenu() {
+      this.$refs['modal-menu'].show()
     },
     logout() {
       this.$refs['modal-logout-confirm'].hide()
